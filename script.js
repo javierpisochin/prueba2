771db2e537e50919e3e62831dedff7355cbf0198
javierpisochin <49105053+javierpisochin@users.noreply.github.com>
2021-06-13 11:51:21 -0300

    mi primer repo

---
+++ script.js
@@ -0,0 +1,35 @@
+function validarEdad(x,y) {
+if(y>=18) {
+
+    return true
+
+} else {
+    return false
+
+
+}
+
+
+
+}
+var nombre = "Diego"
+var edad = 38
+
+var nombre2 = "Javier"
+var edad2 = 44
+var nombre3 = "Andrea"
+var edad3 = 40
+
+var esmayor= validarEdad(nombre,edad)
+
+var otraFuncion = validarEdad( nombre2, edad2)
+console.log (otraFuncion)
+
+var otraFuncion = validarEdad( nombre3, edad3)
+console.log (otraFuncion)
+
+var edadMayor = ( edad, edad1, >  edad2)
+console.log (edadMayor)
+alert(" el usuario: " + nombre + " es: " + esmayor)
+
+
